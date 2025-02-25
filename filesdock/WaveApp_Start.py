7e1a7230b4aa9d2faaea947f0e42107d8f97a6d9
Jan du Plessis <W1009931@MIWAY.CO.ZA>
2025-02-25 08:52:29 +0200

    Initial commit

--- filesdock/WaveApp_Start.py
+++ filesdock/WaveApp_Start.py
@@ -38,7 +38,7 @@ async def serve(q: Q):
     ### Try to connect to the MySQL server, quite important
     print("Connecting to MySQL server")
     conn = mysql.connector.connect(
-            host='hackathon-run-one',
+            host='earnest-vine-451607-f1:us-central1:hackathon-run-one',
             user='patzer',
             password='patzer-forever',
             database='hackathon'
