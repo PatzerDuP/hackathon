5c55e0176b52c56891f8b71a6b464b2ac9ac61ec
Jan du Plessis <W1009931@MIWAY.CO.ZA>
2025-03-03 15:42:10 +0200

    Initial commit

--- filesdock/WaveApp_Start.py
+++ filesdock/WaveApp_Start.py
@@ -11,15 +11,30 @@ database = 'hackathon'
 
 # Main app function to display the connection status
 @app('/hackathon')
+
 async def serve(q: Q):
+
+    q.page['meta'] = ui.meta_card(box='', layouts=[
+        ui.layout(
+            breakpoint='m',
+            zones=[
+                ui.zone('header'),
+                ui.zone('body', direction=ui.ZoneDirection.ROW, zones=[
+                    ui.zone('sidebarL', size='20%'),
+                    ui.zone('content', size='60%'),
+                    ui.zone('sidebarR', size='20%'),
+                ]),
+                ui.zone('footer'),
+            ]
+        )
+    ])
+
     q.page['db_status'] = ui.form_card(
         box=ui.box('content'),
         items=[
             ui.text_xl(f"Database Connection Status: NONE"),
         ]
     )
- 
-    
-    
+
     # Save the page
     await q.page.save()
