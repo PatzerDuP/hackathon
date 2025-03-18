9085c49c10d28759e3c1a15a2f9156df0caccbd2
Jan du Plessis <W1009931@MIWAY.CO.ZA>
2025-03-18 16:07:18 +0200

    Initial commit

--- filesdock/WaveApp_Start.py
+++ filesdock/WaveApp_Start.py
@@ -1,8 +1,40 @@
 from h2o_wave import main, app, Q, ui 
-#import mysql.connector
-import pymysql
-import sqlalchemy
-from google.cloud.sql.connector import Connector, IPTypes
+
+from google.cloud.sql.connector import Connector
+
+import psycopg2
+from google.auth import default
+
+
+INSTANCE_CONNECTION_NAME = 'earnest-vine-451607-f1:us-central1:test-postgres-db' 
+DB_USER = 'postgres'
+DB_PASS = 'test-postgres'
+DB_NAME = 'postgres'
+
+# Connect to PostgreSQL via Cloud SQL
+def get_db_connection():
+    connector = Connector()
+    conn = connector.connect(
+        INSTANCE_CONNECTION_NAME,
+        "postgresql",
+        user=DB_USER,
+        password=DB_PASS,
+        db=DB_NAME,
+    )
+    return conn
+
+def fetch_data_from_db():
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    
+    # Query your database
+    cursor.execute("SELECT * FROM prem_upload")
+    data = cursor.fetchall()
+    
+    cursor.close()
+    connection.close()
+    
+    return data
 
 
 
@@ -72,8 +104,8 @@ async def serve(q: Q):
 
     q.page['readSQL'] = ui.form_card(
         box=ui.box('second_box'), items=[
-        ui.text_xl('File Upload Status'),
-        ui.text('File downloaded successfully.')
+        ui.text_xl('Reading from SQL'),
+        ui.text('Here be the data')
     ])
     # Save this page to update the server side    
     await q.page.save()
