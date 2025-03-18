1ac4622612d2138663ba395abeb3ef02cb408172
Jan du Plessis <W1009931@MIWAY.CO.ZA>
2025-03-18 16:26:58 +0200

    Initial commit

--- filesdock/WaveApp_Start.py
+++ filesdock/WaveApp_Start.py
@@ -6,38 +6,6 @@ import psycopg2
 from google.auth import default
 
 
-INSTANCE_CONNECTION_NAME = 'earnest-vine-451607-f1:us-central1:test-postgres-db' 
-DB_USER = 'postgres'
-DB_PASS = 'test-postgres'
-DB_NAME = 'postgres'
-
-# Connect to PostgreSQL via Cloud SQL
-def get_db_connection():
-    connector = Connector()
-    conn = connector.connect(
-        INSTANCE_CONNECTION_NAME,
-        "postgresql",
-        user=DB_USER,
-        password=DB_PASS,
-        db=DB_NAME,
-    )
-    return conn
-
-def fetch_data_from_db():
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    
-    # Query your database
-    cursor.execute("SELECT * FROM prem_upload")
-    data = cursor.fetchall()
-    
-    cursor.close()
-    connection.close()
-    
-    return data
-
-data = fetch_data_from_db()
-print(data)
 
 # Get feedback when app is started and stopped
 def on_startup():
---
+++ filesdock/connectScript.py
@@ -0,0 +1,39 @@
+from h2o_wave import main, app, Q, ui 
+
+from google.cloud.sql.connector import Connector
+
+import psycopg2
+from google.auth import default
+
+INSTANCE_CONNECTION_NAME = 'earnest-vine-451607-f1:us-central1:test-postgres-db' 
+DB_USER = 'postgres'
+DB_PASS = 'test-postgres'
+DB_NAME = 'postgres'
+
+# Connect to PostgreSQL via Cloud SQL
+def get_db_connection():
+    connector = Connector()
+    conn = connector.connect(
+        INSTANCE_CONNECTION_NAME,
+        "postgresql",
+        user=DB_USER,
+        password=DB_PASS,
+        db=DB_NAME,
+    )
+    return conn
+
+def fetch_data_from_db():
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    
+    # Query your database
+    cursor.execute("SELECT * FROM prem_upload")
+    data = cursor.fetchall()
+    
+    cursor.close()
+    connection.close()
+    
+    return data
+
+data = fetch_data_from_db()
+print(data)
